9d58836876831b43dd78d591485d40acced57549
= <=>
2020-05-19 17:50:26 +0700

    add LoginActivity and activity_login.xml

--- FashionShop/app/src/main/java/com/example/fashionshop/ui/MainActivity.java
+++ FashionShop/app/src/main/java/com/example/fashionshop/ui/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.fashionshop.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +11,19 @@ import com.example.fashionshop.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnStartLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStartLogin = findViewById(R.id.btnStartLogin);
+        btnStartLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
