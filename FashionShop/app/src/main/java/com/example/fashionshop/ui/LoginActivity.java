3dff3700158e4386c670ce870185f4ef41a0d8d9
kingkcode97 <kingkcode@gmail.com>
2020-05-21 14:58:12 +0700

    update Login LoginActivity

--- FashionShop/app/src/main/java/com/example/fashionshop/ui/LoginActivity.java
+++ FashionShop/app/src/main/java/com/example/fashionshop/ui/LoginActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.fashionshop.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -36,6 +42,57 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        EditText editText = findViewById(R.id.username);
+        EditText editText2 = findViewById(R.id.password);
+        TextView usernameWrapper = findViewById(R.id.usernameWrapper);
+        TextView passwordWrapper = findViewById(R.id.passwordWrapper);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (s.length() == 0) {
+                    usernameWrapper.setError("Bạn phải nhập Tên Đăng Nhập");
+                } else {
+                    usernameWrapper.setError(null);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        editText2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (s.length() == 0) {
+                    passwordWrapper.setError("Bạn phải nhập Mật Khẩu");
+                } else {
+                    passwordWrapper.setError(null);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
