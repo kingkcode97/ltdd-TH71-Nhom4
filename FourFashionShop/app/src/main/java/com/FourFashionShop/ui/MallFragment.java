32eae2d4e96a850202335fa5fb0f3f8761b89240
Phuoc <phuoc1999qn1@gmail.com>
2020-05-27 18:22:45 +0700

    Them tablayout trong mall

--- FourFashionShop/app/src/main/java/com/FourFashionShop/ui/MallFragment.java
+++ FourFashionShop/app/src/main/java/com/FourFashionShop/ui/MallFragment.java
@@ -2,64 +2,79 @@ package com.FourFashionShop.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.FourFashionShop.R;
+import com.FourFashionShop.ui.Mall_Item_Fragment.Mall_Item1;
+import com.FourFashionShop.ui.Mall_Item_Fragment.Mall_Item2;
+import com.FourFashionShop.ui.Mall_Item_Fragment.Mall_Item3;
+import com.FourFashionShop.ui.Mall_Item_Fragment.Mall_Item4;
+import com.FourFashionShop.ui.Mall_Item_Fragment.Mall_Item5;
+import com.google.android.material.tabs.TabLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MallFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import Adapter.ViewPagerAdapter;
+
 public class MallFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View view;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     public MallFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MallFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MallFragment newInstance(String param1, String param2) {
-        MallFragment fragment = new MallFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setUpViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    private void setUpViewPager(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager(), 0);
+
+        viewPagerAdapter.addFragment(new Mall_Item1(), "Item 1");
+        viewPagerAdapter.addFragment(new Mall_Item2(), "Item 2");
+        viewPagerAdapter.addFragment(new Mall_Item3(), "Item 3");
+        viewPagerAdapter.addFragment(new Mall_Item4(), "Item 4");
+        viewPagerAdapter.addFragment(new Mall_Item5(), "Item 5");
+
+        viewPager.setAdapter(viewPagerAdapter);
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mall, container, false);
+        view = inflater.inflate(R.layout.fragment_mall, container, false);
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+        return view;
     }
+
 }
